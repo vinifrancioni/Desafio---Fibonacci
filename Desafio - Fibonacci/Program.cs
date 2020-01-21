e8f15aa47b1cf25c8f0fc4ebc6ec6ee784b3cc03
vinifrancioni <vinifrancioni@gmail.com>
2020-01-20 21:45:23 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio - Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass
+{
+    static List<int> Fibonacci()
+    {
+        int a = 0;
+        int b = 1;
+
+        var list = new List<int>();
+
+        while (a < 350)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+            list.Add(a);
+        }
+
+        return list;
+    }
+
+    static void Main()
+    {
+
+        foreach (int valor in Fibonacci())
+        {
+            Console.WriteLine(valor);
+        }
+
+        Console.ReadLine();
+    }
+
+}
